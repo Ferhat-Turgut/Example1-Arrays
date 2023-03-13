40fcfc48adee85d7584d2a9da58bd1b00775efd1
Ferhat-Turgut <turgutferhat.ft@gmail.com>
2023-03-13 16:42:28 +0300

    Add project files.

---
+++ Example1-Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] numbers = new int[10];
+int enterNumber = 0;
+int lengthFlag = 0;
+
+while (lengthFlag < 10)
+{
+    Console.Write($"0 ile 100 arası {lengthFlag + 1}. sayıyı giriniz=");
+    enterNumber = int.Parse(Console.ReadLine());
+
+    if (enterNumber > 0 && enterNumber < 100)
+    {
+        numbers[lengthFlag] = enterNumber;
+        lengthFlag++;
+    }
+    else
+    {
+        Console.WriteLine("Hatalı veri girdiniz.Lütfen kontrol ediniz!");
+    }
+}
+
+Console.WriteLine($"En küçük değer={numbers.Min()}");
+Console.WriteLine($"En büyük değer={numbers.Max()}");
+Console.WriteLine($"Değerlerin Toplamı={numbers.Sum()}");
+Console.WriteLine($"Değerlerin Ortalaması={numbers.Average()}");
